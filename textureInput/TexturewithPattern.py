7add2558d4a233a1b24545bb889691cf2a31027b
moonyuet <64118225+moonyuet@users.noreply.github.com>
2021-03-10 10:46:11 +0800

    Update TexturewithPattern.py

--- textureInput/TexturewithPattern.py
+++ textureInput/TexturewithPattern.py
@@ -59,6 +59,7 @@ class PBR_SHADER(bpy.types.Operator):
         activeObject.data.materials.append(mat_pbr)
         
         bsdf = mat_pbr.node_tree.nodes.get("Principled BSDF")
+        #set specular to zero
         bsdf.inputs[4].default_value =0.0
 
         #diffuse map
